88ee8d5e8c5ef8e8137044f17ae5b9c8f52aab13
Renan Azzolim <azzolimrenan2@gmail.com>
2022-11-27 20:28:18 -0300

    Configuration for reader delimited file - including complex type (Date)

---
+++ src/main/java/com/razzolim/batch/migration/reader/ArquivoDadosBancariosReaderConfig.java
@@ -0,0 +1,25 @@
+package com.razzolim.batch.migration.reader;
+
+import com.razzolim.batch.migration.domain.DadosBancarios;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class ArquivoDadosBancariosReaderConfig {
+
+    @Bean
+    public FlatFileItemReader<DadosBancarios> dadosBancariosReader() {
+        return new FlatFileItemReaderBuilder<DadosBancarios>()
+                .name("dadosBancariosReader")
+                .resource(new FileSystemResource("files/dados_bancarios.csv"))
+                .delimited()
+                .names("pessoaId", "agencia", "conta", "banco", "id")
+                .addComment("--")
+                .targetType(DadosBancarios.class)
+                .build();
+    }
+
+}
